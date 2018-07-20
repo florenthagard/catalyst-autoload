b14bc2a84299d85da059b7725437b5110d82b6d0
Florent <florent.hagard@gmail.com>
2018-07-20 12:08:16 +0200

    add prototypes

---
+++ index.js
@@ -0,0 +1,29 @@
+let modules = {};
+
+Object.defineProperty(String.prototype, "class", {
+	enumerable   : false,
+	configurable : false,
+	get          : function(){
+
+		let context = this.valueOf();
+		if (modules[context]){
+			return modules[context];
+		}	
+			context   = this.valueOf().split('::');
+		let namespace = context.pop();
+			context   = context.shift() || "FWD";
+		let filename  = namespace.split('/').pop();
+
+		if (fs.existsSync(process.env[context] + '/' + namespace + '.js')){
+			let classLoad = require(process.env[context] + '/' + namespace);
+			if (classLoad.name && classLoad.name === filename){
+				return modules[context] = classLoad;
+			}
+		}
+
+		return context + '::' + namespace;
+	}
+});
+
+
+module.exports = String;
