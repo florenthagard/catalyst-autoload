acc49cffbc794b356d7496486aafb47dda991776
Florent <florent.hagard@gmail.com>
2018-07-20 14:55:28 +0200

    fix global access

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ Object.defineProperty(String.prototype, "class", {
 		||  fs.existsSync(process.env[context] + '/' + namespace + '.json')){
 			try {
 				let classLoad = require(process.env[context] + '/' + namespace);
+				console.log(classLoad);
 				if (classLoad.name && classLoad.name === filename){
 					return modules[context] = classLoad;
 				}	return classLoad;
