e0de836ff842cb7cd54a2b7a97d4955d0e662344
Florent <florent.hagard@gmail.com>
2018-07-23 15:12:39 +0200

    fix global access

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ Object.defineProperty(String.prototype, "class", {
 				}
 
 				let Error = /Error:.*'(.*)'/gi.exec(e.stack);
+				console.log(e);
 				if (Error && Error.code === "MODULE_NOT_FOUND"){
 					let indexPathName = pathName +'/index';
 					let basePathName  = pathName +'/'+ path.basename(pathName);
